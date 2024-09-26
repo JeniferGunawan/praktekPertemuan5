45e96327f58ee53097d716c357ee29afe538b6cd
Michael <jenifergunawan5@gmail.com>
2024-09-26 23:11:07 +0700

    update  file app.js dan menambah folder publik berisi file index.html

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const port = 3000;
 // Middleware untuk parsing body request
 app.use(express.json());
 
+// Menyajikan file statis dari folder 'public'
+app.use(express.static('public'));
+
 // Route GET
 app.get('/', (req, res) => {
   res.send('Hello, GET request!');
@@ -17,18 +20,18 @@ app.get('/', (req, res) => {
 
 // Route GET untuk /text
 app.get('/text', (req, res) => {
-    res.send('Ini adalah response text.');
-  });
+  res.send('Ini adalah response text.');
+});
 
 // Route GET untuk /html
 app.get('/html', (req, res) => {
-    res.send('<h1>Ini adalah response HTML</h1>');
-  });
+  res.send('<h1>Ini adalah response HTML</h1>');
+});
 
 // Route GET untuk /json
 app.get('/json', (req, res) => {
-    res.json({ message: 'Ini adalah response JSON' });
-  });
+  res.json({ message: 'Ini adalah response JSON' });
+});
 
 // Route POST
 app.post('/submit', (req, res) => {
